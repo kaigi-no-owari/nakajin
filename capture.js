4878a3b6bb5f9044df3ed09a9f8dcce1453ae753
radiocat <radiocat.app@gmail.com>
2016-06-21 01:23:33 +0900

    CasperJS sample

---
+++ capture.js
@@ -0,0 +1,16 @@
+var casper = require('casper').create({});
+var url = "http://www.yahoo.co.jp/";
+
+casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.10 Safari/537.36 OPR/39.0.2256.4 (Edition beta)');
+
+casper.start(url, function() {
+    this.echo('access ' + url);
+});
+
+casper.wait(3000);
+
+casper.then(function() {
+    this.capture('capture-yahoo.png');
+});
+
+casper.run();
